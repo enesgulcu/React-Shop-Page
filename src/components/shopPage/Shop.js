c3e1effd1de45eae146679209cc8e1d0168ba59a
enesgulcu <enes.gulcu@hotmail.com>
2022-03-11 14:46:30 +0300

    Filtered prodcut using textBox on navbar

--- src/components/shopPage/Shop.js
+++ src/components/shopPage/Shop.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {useState, useEffect} from 'react';
-import SearchList from './SearchList';
 import Products from './Products';
 import Catagories from './Catagories';
 import Data from './Data.json'
@@ -10,32 +9,35 @@ function Shop() {
 
   const [data, setData] = useState(Data);
   const [filterCatagories, setFilterCatagories] = useState("All");
+  const [textSearch, setTextSearch] = useState("");
 
+ // Catagories Filter
  // eğer filterCatagories = ALL (hepsini getir) : Değilse => filterCatagories ile data nın eşleşenlerini getirir.
   const filterData = data.filter((data)=> filterCatagories === "All" ? data : data.catagories === filterCatagories)
-
+  // Text Filter
+  const searchFilter =  filterData.filter((search)=> search.title.toLowerCase().includes(textSearch.toLowerCase()));
   
-  
-  
-
-
+  const onChangeImput = (e) => {
+    setTextSearch(e.target.value);       
+  }; 
+            
   return (
     <div>
         <div className="container-fluid">
           <div className="row">
             <div className="col-12 bg-dark nav_up">
               <a href="#">LOGO</a>
-              <input type="text" className='searchConsole' placeholder='Your key Word'/>
+              <input type="text" className='searchConsole' placeholder='Your key Word' onChange={onChangeImput}/>
             </div>
             <div className='under_box'>
             <div className="catagori_box">
-              <div className='catagories '>
+              <div className='catagories'>
               <Catagories allData = {data} setFilterCatagories = {setFilterCatagories}/>  
               </div>
             </div>  
             <div className="product_box">
               <div className="prodcuts">
-              <Products Product = {filterData}/>
+              <Products Product = {searchFilter}/>
               </div>
             </div>
             </div>
