4ca7f5f15341b8bfdbe30ce74405fca98122f350
enesgulcu <enes.gulcu@hotmail.com>
2022-03-20 20:13:53 +0300

    api, configured

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { useState, useEffect } from 'react';
+import UsersCSS from './Users.css'
 import {BrowserRouter as Router, Routes, Route, Link, Outlet, NavLink, useParams} from 'react-router-dom'
 function Users() {
 
@@ -12,7 +13,7 @@ function Users() {
         await axios('https://randomuser.me/api/?nat=tr')
         .then((response)=>{
           console.log(response.data.results);
-          setUsers(...users, response.data.results);  
+          setUsers(response.data.results);  
         })
     }    
     catch (err) {
@@ -28,22 +29,32 @@ function Users() {
 
 
   return (
-    <div>
+    <div className='userMain'>
         <ul>
           {users.map((user, index)=>{
-            return <li key={index}>
+            return <li key={index} className='UserProfile'>
               <div className="profile">
-                <div className="image">
+
+                <div className="user-image">
                   <img src={user.picture.large} alt="" />
                 </div>
-                <div className="user-details">
-                  <h5>{user.name.first} {user.name.last}</h5>
-                  <div className="gender">{user.gender === 'female' ? 'Kadın' : 'Erkek'}</div>
-                  <div className="city">{user.location.city}</div>
+
+                <div className="user-details">                  
+                  <h5>
+                    <span className="gender">{user.gender === 'female' ? 'Mrs' : 'Mr'} </span>
+                    {user.name.first} {user.name.last}
+                  </h5>   
+                  <div className="city">City: {user.location.city}</div>
+                  <div className="Phone">Phone: {user.phone}</div>
+                  <div className="userName">user Name: {user.login.username}</div>     
+                  <div className="userPass">Password: {user.login.password}</div>                
                 </div>
+                <button className='changeUser' onClick={getData}>Change User</button>
               </div>
             </li>
           })}
+
+          
         </ul>
          
     </div>
