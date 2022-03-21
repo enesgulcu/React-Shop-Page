08d0073e4e5f4d6f2bbc0a520a16eaf905be6264
enesgulcu <enes.gulcu@hotmail.com>
2022-03-21 19:27:08 +0300

    User selection and display on the shop page

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,14 @@ import Shop from './shopPage/Shop'
 import NavBar from './NavBar'
 import Users from './Users'
 import Footer from './Footer'
+import { useState, useEffect } from 'react';
 
 import {BrowserRouter as Router, Routes, Route, Link, Outlet, NavLink, useParams} from 'react-router-dom'
 function Main() {
+  const [activeUser, setActiveUser] = useState(false);
+  const [users, setUsers] = useState([]); 
+  const [chosedUser, setchosedUser] = useState([]); 
+  
   return (
     <div>
           
@@ -13,8 +18,9 @@ function Main() {
       <Router>
         <NavBar/>
           <Routes>
-              <Route exact path={"/"} element={<Shop/>}/>  
-              <Route path={"/users"} element={<Users/>}/>                                    
+              <Route exact path={"/"} element={<Shop activeUser={activeUser} setActiveUser={setActiveUser}  users={chosedUser} />}/>  
+              <Route path={"/users"} 
+              element={<Users users={users} setUsers={setUsers} setActiveUser={setActiveUser} activeUser={activeUser} chosedUser={chosedUser} setchosedUser={setchosedUser} />}/>                                    
           </Routes>
           <Footer/>
         </Router>
